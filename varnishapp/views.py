d83bb3af238f2a194a9a6c5b9be3efaeb959a173
Guillaume Pousseo <guillaumepousseo@revsquare.com>
2015-03-11 12:53:17 -1000

    making django >= 1.6 compliant

--- varnishapp/views.py
+++ varnishapp/views.py
@@ -3,11 +3,13 @@ from manager import manager
 from django.shortcuts import render
 from django.conf import settings
 
+
 def get_stats():
     stats = [x[0] for x in manager.run('stats')]
     return zip(getattr(settings, 'VARNISH_MANAGEMENT_ADDRS', ()), stats)
-    
-def management(request): 
+
+
+def management(request):
     if not request.user.is_superuser:
         return HttpResponseRedirect('/admin/')
     if 'command' in request.REQUEST:
@@ -19,9 +21,8 @@ def management(request):
         errors = {}
     except:
         stats = None
-        errors = {"stats":"Impossible to access the stats for server : %s" \
-                  %getattr(settings, 'VARNISH_MANAGEMENT_ADDRS', ())}
-        
-    extra_context = {'stats':stats,
-                     'errors':errors}
+        errors = {"stats": "Impossible to access the stats for server : %s"
+                  % getattr(settings, 'VARNISH_MANAGEMENT_ADDRS', ())}
+
+    extra_context = {'stats': stats, 'errors': errors}
     return render(request, 'varnish/report.html', extra_context)
