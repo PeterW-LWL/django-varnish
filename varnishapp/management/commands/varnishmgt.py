cb5826782a24e4bee57c4864c9259a56a3993e38
Guillaume Pousseo <guillaumepousseo@revsquare.com>
2016-04-08 16:54:51 +0200

    Attempt to comply with django 1.8

--- varnishapp/management/commands/varnishmgt.py
+++ varnishapp/management/commands/varnishmgt.py
@@ -2,7 +2,11 @@ from django.core.management.base import BaseCommand
 from varnishapp.manager import manager
 from pprint import pprint
 
+
 class Command(BaseCommand):
+    def add_arguments(self, parser):
+        parser.add_argument('args')
+
     def handle(self, *args, **options):
         if args:
             pprint(manager.run(*args))
