a7f57a96d351d23c4df842ebbf01e2c8adabcbac
Peter Wischer <peter.wischer@lwl.org>
2021-04-14 14:46:24 +0200

    test logging

--- varnishapp/recivers.py
+++ varnishapp/recivers.py
@@ -14,17 +14,22 @@ def connect():
     Connect signal recivers.
     """
     for model in getattr(settings, 'VARNISH_WATCHED_MODELS', ()):
+        model_class = import_string(model)
         logger.debug("Setting up `post_save` singal handler for %s", model)
-        print(model)
+        print(model, model_class)
         post_save.connect(
             absolute_url_purge_handler,
-            sender=import_string(model)
+            sender=model_class,
         )
 
 
 def absolute_url_purge_handler(sender, **kwargs):
     instance = kwargs['instance']
     if hasattr(instance, 'get_absolute_url'):
-        logger.debug("Purging %s of %s", instance.get_absolute_url(), instance)
-        print(instance)
+        logger.debug("Purging %s for %s",
+                     instance.get_absolute_url(), instance)
+        print("Success", instance)
         manager.run('purge.url', r'^%s$' % instance.get_absolute_url())
+    else:
+        logger.debug("Purging failed for %s", instance)
+        print("Failed", instance)
