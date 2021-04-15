92bbd22915e2aa0d597cda9cf848b2c56029a7c1
Peter Wischer <peter.wischer@lwl.org>
2021-04-15 07:32:22 +0200

    wip

--- varnishapp/recivers.py
+++ varnishapp/recivers.py
@@ -14,13 +14,37 @@ def connect():
     Connect signal recivers.
     """
     for model in getattr(settings, 'VARNISH_WATCHED_MODELS', ()):
+        url_accessor = None
+        if isinstance(model, tuple):
+            model, url_accessor = model
         model_class = import_string(model)
         logger.debug("Setting up `post_save` singal handler for %s", model)
-        print(model, model_class)
-        post_save.connect(
-            absolute_url_purge_handler,
-            sender=model_class,
-        )
+        print(model, model_class, url_accessor)
+        if url_accessor is None:
+            post_save.connect(
+                absolute_url_purge_handler,
+                sender=model_class,
+            )
+        else:
+            post_save.connect(
+                create_handler(url_accessor),
+                sender=model_class,
+            )
+
+
+def create_handler(accessor_method):
+    def inner(sender, **kwargs):
+        instance = kwargs['instance']
+        method = getattr(instance, accessor_method, None)
+        if method:
+            logger.debug("Calling %s on %s", accessor_method, instance)
+            purge_url = method()
+            logger.debug("Purging %s for %s", purge_url, instance)
+            manager.run('purge.url', r'^%s$' % purge_url)
+            print("Success", instance, accessor_method)
+        else:
+            logger.error("%s has no method %s", instance, accessor_method)
+            print("Failed", instance, accessor_method)
 
 
 def absolute_url_purge_handler(sender, **kwargs):
@@ -28,8 +52,8 @@ def absolute_url_purge_handler(sender, **kwargs):
     if hasattr(instance, 'get_absolute_url'):
         logger.debug("Purging %s for %s",
                      instance.get_absolute_url(), instance)
-        print("Success", instance)
         manager.run('purge.url', r'^%s$' % instance.get_absolute_url())
+        print("Success", instance)
     else:
         logger.debug("Purging failed for %s", instance)
         print("Failed", instance)
